c3a30ede0895becd0644fcb91e2c90e2b4e975b0
Firas ABDELAZIZ <firasabdelaziz123@gmail.com>
2023-12-24 01:53:10 +0100

    feat(gateway) : setup gateway project feat(script) : create two script in goal to manage ms containers projects microservices

---
+++ startMicroservices.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Array of directories containing docker-compose.yml files
+directories=(
+    "./bookService"
+    "./gatewayService"
+    "./socialService"
+    "./userService"
+    # Add more directories as needed
+)
+
+echo "==========================="
+echo "🚀 Starting microservices 🚀"
+echo "Your computer is having a hard time running all the microservices at once. But don't worry, I've got you covered! 😊"
+echo "I will create local-compose.yml files for you, each with its own database. 📝"
+echo "We'll use Nginx as part of the PHP image for a more optimized setup. 🌟"
+
+echo "Do you want to create a new network? (This is useful if it's your first time running the microservices) 🌐 [Y/n]"
+
+read -r network_reply
+if [[ $network_reply =~ ^[Yy]$ ]]; then
+    echo "==========================="
+    echo "Creating a new network... ⚡️"
+    docker network create todo-network
+    echo "New network 'mynetwork' created successfully! 🎉"
+fi
+
+echo "Do you want to use local-compose.yml files? (These files provide better resource management) 📄 [Y/n]"
+
+read -r compose_reply
+if [[ $compose_reply =~ ^[Yy]$ ]]; then
+    echo "==========================="
+    echo "Starting microservices with local-compose.yml... 🚀"
+    for dir in "${directories[@]}"; do
+        echo "📂 Starting microservices in directory: $dir"
+        cd "$dir" || exit
+        pwd
+        docker-compose -f local-compose.yml up -d --build --remove-orphans
+        # docker-compose -f local-compose.yml logs -f &
+        echo "==========================="
+        echo "🛑 Docker-compose exited with status $?" 
+        cd ..
+    done
+else
+    echo "==========================="
+    echo "Starting microservices with docker-compose.yml... 🚀"
+    for dir in "${directories[@]}"; do
+        echo "📂 Starting microservices in directory: $dir"
+        cd "$dir" || exit
+        pwd
+        docker-compose up -d --build --remove-orphans
+        # docker-compose logs -f &
+        echo "==========================="
+        echo "🛑 Docker-compose exited with status $?" 
+        cd ..
+    done
+fi
+
+echo "==========================="
+echo "Microservices deployment completed! 🎉"
---
+++ stopMicroservices.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Array of directories containing microservices
+directories=(
+    "./bookService"
+    "./gatewayService"
+    "./socialService"
+    "./userService"
+    # Add more directories as needed
+)
+echo  "What You use local-compose ? [Y/n] 🌀 "
+read REPLY 
+echo $REPLY
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "==========================="
+    echo "Stoping microservices with local-compose.yml"
+    for dir in "${directories[@]}"
+    do
+        echo "Stoping microservices in directory: $dir"
+        cd "$dir"
+        pwd
+        docker-compose -f local-compose.yml down
+            echo "==========================="
+            echo "Docker-compose exited with status $?" 
+        cd ..
+    done
+else
+    echo "==========================="
+    echo "Stoping microservices with docker-compose.yml"
+    for dir in "${directories[@]}"
+    do
+        echo "Stoping microservices in directory: $dir"
+        cd "$dir"
+        pwd
+        docker-compose down
+            echo "==========================="
+            echo "Docker-compose exited with status $?" 
+        cd ..
+    done
+fi
+
+echo "==========================="
